f52dfe4f27558c64dcc7bd5e155dd0bf4170c9ae
VsHrom <dudnikvad@gmail.com>
2015-04-01 19:16:06 +0200

    add new class

---
+++ SomeCLass.java
@@ -0,0 +1,12 @@
+package com.example.lb5;
+
+/**
+ * Created by Вадим on 01.04.2015.
+ */
+public class SomeCLass {
+    private String sometext;
+
+    public SomeCLass(String sometext) {
+        this.sometext = sometext;
+    }
+}
